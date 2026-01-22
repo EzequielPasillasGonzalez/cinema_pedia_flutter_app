22751bda86ec4d119f7daaf8bfdcf44fd0e6a6c6
Ezequiel <ingepasillasg@gmail.com>
2026-01-21 18:02:51 -0600

    feat: Modulo AppTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cinema_pedia_app/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
