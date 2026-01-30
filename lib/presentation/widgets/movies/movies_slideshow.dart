627a7c6566784c3c6461078c376acedd19a9b5c5
Ezequiel <ingepasillasg@gmail.com>
2026-01-29 23:04:04 -0600

    feat: paginacion en movies slide

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -3,35 +3,86 @@ import 'package:cinema_pedia_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
-class MoviesSlideshow extends StatelessWidget {
+class MoviesSlideshow extends StatefulWidget {
   final List<Movie> movies;
-
-  final CardSwiperController cardSwiperController = CardSwiperController();
 
   MoviesSlideshow({super.key, required this.movies});
 
   @override
+  State<MoviesSlideshow> createState() => _MoviesSlideshowState();
+}
+
+class _MoviesSlideshowState extends State<MoviesSlideshow> {
+  final CardSwiperController cardSwiperController = CardSwiperController();
+
+  int currentIndex = 0;
+
+  @override
+  void dispose() {
+    cardSwiperController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 210,
-      width: double.infinity,
-      child: CardSwiper(
-        cardBuilder:
-            (
-              context,
-              index,
-              horizontalOffsetPercentage,
-              verticalOffsetPercentage,
-            ) {
-              final movie = movies[index];
-              return MoviesCard(movie: movie);
+    final colors = Theme.of(context).colorScheme;
+
+    final Size size = MediaQuery.of(context).size;
+
+    return Column(
+      children: [
+        SizedBox(
+          height: 210,
+          width: double.infinity,
+          child: CardSwiper(
+            cardBuilder:
+                (
+                  context,
+                  index,
+                  horizontalOffsetPercentage,
+                  verticalOffsetPercentage,
+                ) {
+                  final movie = widget.movies[index];
+                  return MoviesCard(movie: movie);
+                },
+            onSwipe: (previousIndex, index, direction) {
+              currentIndex = index ?? 0;
+              setState(() {});
+              return true;
             },
-        cardsCount: movies.length,
-        controller: cardSwiperController,
-        numberOfCardsDisplayed: 3,
-        backCardOffset: const Offset(130, 0),
-        padding: const EdgeInsets.only(right: 30, left: 20),
-      ),
+            onUndo: (previousIndex, index, direction) {
+              currentIndex = index;
+              setState(() {});
+              return true;
+            },
+            cardsCount: widget.movies.length,
+            controller: cardSwiperController,
+            numberOfCardsDisplayed: 3,
+            backCardOffset: const Offset(130, 0),
+            padding: const EdgeInsets.only(right: 30, left: 20),
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.movies.length, (index) {
+            final isSelected = currentIndex == index;
+
+            return AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              width: isSelected ? 20 : 10,
+              height: 10,
+              decoration: BoxDecoration(
+                color: isSelected ? colors.primary : colors.secondary,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            );
+          }),
+        ),
+      ],
     );
   }
 }
